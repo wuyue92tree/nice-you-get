17e60943ca64e0a0a1e9810305e9f91cf6ba6550
wuyue <wuyue92tree@163.com>
2021-05-14 22:08:10 +0800

    add logger

--- conf/settings.py
+++ conf/settings.py
@@ -1,6 +1,15 @@
 # -*- coding: UTF-8 -*-
 import os
+import sys
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+if sys.platform == 'win32':
+    HOME_DIR = os.path.join(os.path.expanduser('~'), '.nice_you_get')
+else:
+    HOME_DIR = os.path.join(os.environ['HOME'], '.nice_you_get')
+
+if os.path.exists(HOME_DIR) is False:
+    os.makedirs(HOME_DIR)
 
 VERSION = '1.1.0'
--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-from you_get.__main__ import main
+import sys
+import os
+import you_get
+from PySide2.QtWidgets import QApplication, QLabel
+from windows import MainWindow
+from utils.logger import get_logger
+
+logger = get_logger()
+
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) > 1:
+        you_get.main()
+    else:
+        logger.info(f'process start at: {os.getpid()}')
+        app = QApplication(sys.argv)
+        main_window = MainWindow()
+        main_window.show()
+        sys.exit(app.exec_())
