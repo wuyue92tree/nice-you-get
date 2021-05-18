6b844eae76409d49ff366a073fa820794c87c8f8
wuyue <wuyue92tree@163.com>
2021-05-18 18:07:21 +0800

    use tableView replace tableWidget & update filter logic

--- windows/__init__.py
+++ windows/__init__.py
@@ -3,9 +3,10 @@ import sys
 import os
 import json
 from io import StringIO
-from PySide2 import QtGui
-from PySide2.QtWidgets import QAbstractItemView, QFileDialog, QHeaderView, QMainWindow, QStyle, QStyleOptionButton, QTableWidgetItem
-from PySide2.QtCore import QRect, QThread, QUrl, Qt, Signal, Slot
+from PySide2 import QtCore
+from PySide2.QtGui import QDesktopServices, QStandardItemModel, QStandardItem
+from PySide2.QtWidgets import QAbstractItemView, QFileDialog, QHeaderView, QMainWindow, QStyle, QStyleOptionButton
+from PySide2.QtCore import QRect, QRegExp, QThread, QUrl, Qt, Signal, Slot, QSortFilterProxyModel
 from windows.ui import mainwindow, parsedwindow, downloadwindow
 from utils.logger import get_logger
 from utils.config import config
@@ -100,43 +101,70 @@ class QCheckableHeaderView(QHeaderView):
         return self.isOn
 
 
+class MultiSortFilterProxyModel(QSortFilterProxyModel):
+    def __init__(self, *args, **kwargs):
+        QSortFilterProxyModel.__init__(self, *args, **kwargs)
+        self.filters = {}
+
+    def setFilterByColumn(self, regex, column):
+        self.filters[column] = regex
+        self.invalidateFilter()
+
+    def filterAcceptsRow(self, source_row, source_parent):
+        for key, regex in self.filters.items():
+            ix = self.sourceModel().index(source_row, key, source_parent)
+            if ix.isValid():
+                text = self.sourceModel().data(ix)
+                if not regex.exactMatch(text):
+                    return False
+        return True
+
+
 class ParsedWindow(QMainWindow, parsedwindow.Ui_MainWindow):
     def __init__(self, parent=None, is_playlist=False):
         super().__init__(parent)
         self.setupUi(self)
         self.parse_thread = ParseThread(self)
         
-        self.table_titles = ['标题', '站点', '清晰度', '格式', '大小']
+        self.table_titles = ['标题', '站点', '清晰度', '媒体格式', '格式', '大小']
         self.format_index = 3
+        self.size_index = 5
         if is_playlist:
-            self.table_titles = ['标题', '列表标题', '站点', '清晰度', '格式', '大小']
+            self.table_titles = ['标题', '列表标题', '站点', '清晰度','媒体格式', '格式', '大小']
             self.format_index = 4
-        
-        self.parsedTableWidget.setColumnCount(len(self.table_titles))
-        for index, table_title in enumerate(self.table_titles):
-            self.parsedTableWidget.setHorizontalHeaderItem(index, QTableWidgetItem(table_title))
+            self.size_index = 6
 
-        # parsedTableWidget
+        # parsedTableView
+        self.model = QStandardItemModel(self)
+        self.model.setHorizontalHeaderLabels(self.table_titles)
+        
+        self.proxy = MultiSortFilterProxyModel(self)
+        self.proxy.setSourceModel(self.model)
+
         self.parsedCheckableHeaderView = QCheckableHeaderView(Qt.Horizontal, self)
         self.parsedCheckableHeaderView.setSectionsClickable(True)
-        self.parsedCheckableHeaderView.sectionClicked.connect(self.call_update_section)
-        self.parsedTableWidget.setHorizontalHeader(self.parsedCheckableHeaderView)
-        self.parsedTableWidget.verticalHeader().setHidden(True) # 隐藏默认的行号
-        self.parsedTableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch) # 设置自适应宽度
-        self.parsedTableWidget.setEditTriggers(QAbstractItemView.NoEditTriggers)    # 禁用表格编辑
-        self.parsedTableWidget.setSelectionBehavior(QAbstractItemView.SelectRows)   # 设置item选中时选择整行
-        self.parsedTableWidget.itemChanged.connect(self.call_update_checkbox)
+        self.parsedCheckableHeaderView.sectionClicked.connect(self.on_parsedCheckableHeaderView_sectionClicked)
+        self.parsedTableView.setHorizontalHeader(self.parsedCheckableHeaderView)
+        self.parsedTableView.verticalHeader().setHidden(True) # 隐藏默认的行号
+        self.parsedTableView.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch) # 设置自适应宽度
+        self.parsedTableView.setEditTriggers(QAbstractItemView.NoEditTriggers)    # 禁用表格编辑
+        self.parsedTableView.setSelectionBehavior(QAbstractItemView.SelectRows)   # 设置item选中时选择整行
+        
+        self.parsedTableView.setModel(self.proxy)
+        self.model.itemChanged.connect(self.on_model_itemChanged)
         self.sort_asc = False   # 排序
         self.global_checkbox = Qt.Unchecked # 全局checkbox选择器
 
-        self.parse_thread.finished.connect(self.call_update_table)
+        self.parse_thread.finished.connect(self.on_parse_thread_finished)
 
-        self.current_format = 0
         self.format_list = []
-        self.formatComboBox.currentIndexChanged.connect(self.call_filter_by_format)
+        self.formatComboBox.currentIndexChanged.connect(self.on_formatComboBox_currentIndexChanged)
+        self.container_list = []
+        self.containerComboBox.currentIndexChanged.connect(self.on_containerComboBox_currentIndexChanged)
+        self.searchLineEdit.textChanged.connect(self.on_searchLineEdit_textChanged)
 
-        self.reparsePushButton.clicked.connect(self.call_reparse)
-        self.downloadSelectedPushButton.clicked.connect(self.call_download)
+        self.reparsePushButton.clicked.connect(self.on_reparsePushButton_clicked)
+        self.downloadSelectedPushButton.clicked.connect(self.on_downloadSelectedPushButton_clicked)
 
         self.download_task_list = []
 
@@ -149,42 +177,73 @@ class ParsedWindow(QMainWindow, parsedwindow.Ui_MainWindow):
         logger.info(f'start parse: {url}')
         self._parse(url)
         
-    def call_reparse(self):
+    def on_reparsePushButton_clicked(self):
         url = self.linkValueLabel.text()
-        self.parsedTableWidget.clearContents()
-        self.parsedTableWidget.setRowCount(0)
+        self.model.removeRows( 0, self.model.rowCount())
+
+        self.searchLineEdit.clear()
+
         self.formatComboBox.clear()
-        self.formatComboBox.addItem('格式')
+        self.formatComboBox.addItem('媒体格式')
+
+        self.containerComboBox.clear()
+        self.containerComboBox.addItem('格式')
+
         logger.info(f'start reparse: {url}')
         self._parse(url)
 
     def call_filter_table(self):
-        rows = self.parsedTableWidget.rowCount()
-        if self.formatComboBox.currentIndex() == 0:
+        rows = self.model.rowCount()
+        if self.formatComboBox.currentIndex() == 0 and self.containerComboBox.currentIndex()==0:
             for row in range(rows):
-                self.parsedTableWidget.setRowHidden(row, False)
-                self.parsedTableWidget.item(row, 0).setCheckState(self.global_checkbox)
+                self.parsedTableView.setRowHidden(row, False)
+                self.model.item(row, 0).setCheckState(self.global_checkbox)
         else:
             format = self.formatComboBox.itemText(self.formatComboBox.currentIndex())
+            container = self.containerComboBox.itemText(self.containerComboBox.currentIndex())
             for row in range(rows):
-                if self.parsedTableWidget.item(row, self.format_index).text() == format:
-                    self.parsedTableWidget.setRowHidden(row, False)
-                    self.parsedTableWidget.item(row, 0).setCheckState(self.global_checkbox)
+                if self.model.item(row, self.format_index).text() == format \
+                    or self.model.item(row, self.format_index+1).text() == container:
+                    self.parsedTableView.setRowHidden(row, False)
+                    self.model.item(row, 0).setCheckState(self.global_checkbox)
                 else:
-                    item = self.parsedTableWidget.item(row, 0)
+                    item = self.model.item(row, 0)
                     if item.checkState() == Qt.Checked:
                         item.setCheckState(Qt.Unchecked)
-                    self.parsedTableWidget.setRowHidden(row, True)
+                    self.parsedTableView.setRowHidden(row, True)
 
-    def call_filter_by_format(self):
+    def set_global_checkbox2unchecked(self):
         # 切换过滤条件时，强制将所有checkbox值为unchecked
         self.global_checkbox = Qt.Unchecked
         self.parsedCheckableHeaderView.isOn = False
         self.parsedCheckableHeaderView.updateSection(0)
 
-        self.call_filter_table()
 
-    def call_update_table(self, data):
+    def on_formatComboBox_currentIndexChanged(self, index):
+        self.set_global_checkbox2unchecked()
+        filter = QRegExp(
+            self.formatComboBox.itemText(index) if index != 0 else '.*',  
+            QtCore.Qt.CaseInsensitive, QtCore.QRegExp.RegExp
+        )
+        self.proxy.setFilterByColumn(filter, self.format_index)
+        
+    def on_containerComboBox_currentIndexChanged(self, index):
+        self.set_global_checkbox2unchecked()
+        filter = QRegExp(
+            self.containerComboBox.itemText(index) if index != 0 else '.*',  
+            QtCore.Qt.CaseInsensitive, QtCore.QRegExp.RegExp
+        )
+        self.proxy.setFilterByColumn(filter, self.format_index+1)
+
+    def on_searchLineEdit_textChanged(self, text):
+        self.set_global_checkbox2unchecked()
+        search = QRegExp(
+            f'*{text}' if text != '' else '.*',
+            QtCore.Qt.CaseInsensitive, QtCore.QRegExp.RegExp
+        )
+        self.proxy.setFilterByColumn(search, 0)
+
+    def on_parse_thread_finished(self, data):
         data = json.loads(data)
         rows = len(data.get('streams'))
         streams_keys = list(data.get('streams').keys())
@@ -192,37 +251,44 @@ class ParsedWindow(QMainWindow, parsedwindow.Ui_MainWindow):
 
         self.format_list += streams_keys
         self.format_list.sort()
-        self.formatComboBox.addItems(set(self.format_list))
 
         for row in range(rows):
-            row = self.parsedTableWidget.rowCount()
-            self.parsedTableWidget.insertRow(row)
             for col, title in enumerate(self.table_titles):
                 if title == '标题':
-                    item = QTableWidgetItem(data.get('title'))
+                    item = QStandardItem(data.get('title'))
                     item.setCheckState(Qt.Unchecked)
+                    item.setCheckable(True)
                     item.setToolTip(data.get('title'))
                 elif title == '站点':
-                    item = QTableWidgetItem(data.get('site'))
+                    item = QStandardItem(data.get('site'))
                     item.setTextAlignment(Qt.AlignCenter)
                 elif title == '清晰度':
-                    item = QTableWidgetItem(streams_values[row].get('quality'))
+                    item = QStandardItem(streams_values[row].get('quality'))
+                    item.setTextAlignment(Qt.AlignCenter)
+                elif title == '媒体格式':
+                    item = QStandardItem(streams_keys[row])
+                    item.setTextAlignment(Qt.AlignCenter)
                 elif title == '格式':
-                    item = QTableWidgetItem(streams_keys[row])
+                    item = QStandardItem(streams_values[row].get('container'))
+                    item.setTextAlignment(Qt.AlignCenter)
+                    self.container_list.append(item.text())
                 elif title == '大小':
-                    item = QTableWidgetItem()
-                    # 通过setData方法解决排序错误问题
-                    item.setData(Qt.EditRole, streams_values[row].get('size'))
+                    item = QStandardItem(str(streams_values[row].get('size')))
+                    # 通过setData方法解决排序错误问题 (使用tableWidget时存在此问题)
+                    # item.setData(Qt.EditRole, streams_values[row].get('size'))
                 elif title == '列表标题':
-                     item = QTableWidgetItem(streams_keys[row])
-                self.parsedTableWidget.setItem(row, col, item)
+                     item = QStandardItem(streams_keys[row])
+                self.model.setItem(row, col, item)
 
-        self.parsedTableWidget.horizontalHeader().setSortIndicatorShown(True)
-        self.parsedTableWidget.sortByColumn(self.format_index + 1, Qt.DescendingOrder)
+        self.formatComboBox.addItems(set(self.format_list))
+        self.containerComboBox.addItems(set(self.container_list))
+
+        self.parsedTableView.horizontalHeader().setSortIndicatorShown(True)
+        self.parsedTableView.sortByColumn(self.size_index, Qt.DescendingOrder)
         self.statusBar().showMessage(f'解析完成')
         logger.info(f'parse finished: {self.linkValueLabel.text()}')
 
-    def call_update_checkbox(self, item):
+    def on_model_itemChanged(self, item):
         check_state = item.checkState()
         if check_state == Qt.Checked:
             stream_id = item.row()
@@ -231,8 +297,8 @@ class ParsedWindow(QMainWindow, parsedwindow.Ui_MainWindow):
             show_rows = 0
 
             # 当显示行数与被选中行数一致时，将全局checkbox置为checked
-            for row in range(self.parsedTableWidget.rowCount()):
-                if self.parsedTableWidget.isRowHidden(row) is False:
+            for row in range(self.model.rowCount()):
+                if self.parsedTableView.isRowHidden(row) is False:
                     show_rows += 1
             if len(self.download_task_list) == show_rows:
                 if self.global_checkbox == Qt.Unchecked:
@@ -252,39 +318,51 @@ class ParsedWindow(QMainWindow, parsedwindow.Ui_MainWindow):
         status = self.parsedCheckableHeaderView.updateCheckBox()
         self.global_checkbox = Qt.Checked if status else Qt.Unchecked
 
-    def call_update_section(self, logicalIndex):
-        if logicalIndex == 4:
+    def on_parsedCheckableHeaderView_sectionClicked(self, logicalIndex):
+        if logicalIndex == self.size_index:
             # 通过 大小 列进行排序，默认desc
-            self.parsedTableWidget.horizontalHeader().setSortIndicatorShown(True)
+            self.parsedTableView.horizontalHeader().setSortIndicatorShown(True)
             if self.sort_asc == False:
-                self.parsedTableWidget.sortByColumn(logicalIndex, Qt.AscendingOrder)
+                self.parsedTableView.sortByColumn(logicalIndex, Qt.AscendingOrder)
             else:
-                self.parsedTableWidget.sortByColumn(logicalIndex, Qt.DescendingOrder)
+                self.parsedTableView.sortByColumn(logicalIndex, Qt.DescendingOrder)
             self.sort_asc = not self.sort_asc
         else:
+            self.parsedTableView.horizontalHeader().setSortIndicatorShown(False)
             # 第一列表头被点击时，切换全局checkbox状态
             if logicalIndex == 0:
                 self.update_global_checkbox()
                 self.call_filter_table()
-            self.parsedTableWidget.horizontalHeader().setSortIndicatorShown(False)
 
-    def call_download(self):
+    def on_downloadSelectedPushButton_clicked(self):
         logger.info(f'got {len(self.download_task_list)} download task: {self.download_task_list}')
-        for stream_id in self.download_task_list:
-            title = self.parsedTableWidget.item(stream_id, 0).text()
-            link = self.linkValueLabel.text()
-            format = self.parsedTableWidget.item(stream_id, self.format_index).text()
-            download_window = DownloadWindow(self, title, link, format)
+        if len(self.download_task_list) > 1:
+            self.statusBar().showMessage('暂不支持多个媒体同时下载')
+            return 
+        for row in self.download_task_list:
+            download_window = DownloadWindow(self, row)
             download_window.show()
 
 
 class DownloadWindow(QMainWindow, downloadwindow.Ui_MainWindow):
-    def __init__(self, parent, title, link, format):
+    def __init__(self, parent, row):
         super(DownloadWindow, self).__init__(parent)
         self.setupUi(self)
-        self.titleValueLabel.setText(title)
-        self.linkValueLabel.setText(link)
-        self.formatValueLabel.setText(format)
+        self.titleValueLabel.setText(
+            self.parent().model.item(row, 0).text()
+        )
+        self.linkValueLabel.setText(
+            self.parent().linkValueLabel.text()
+        )
+        self.qualityValueLabel.setText(
+            self.parent().model.item(row, self.parent().format_index-1).text()
+        )
+        self.formatValueLabel.setText(
+            self.parent().model.item(row, self.parent().format_index).text()
+        )
+        self.containerValueLabel.setText(
+            self.parent().model.item(row, self.parent().format_index+1).text()
+        )
 
         self.download_thread = DownloadThread(self)
         self.statusBar().showMessage('开始下载')
@@ -352,7 +430,7 @@ class MainWindow(QMainWindow, mainwindow.Ui_MainWindow):
         self.option_window.show()
 
     def render_about_window(self):
-        QtGui.QDesktopServices.openUrl(QUrl('https://github.com/wuyue92tree/nice-you-get'))
+        QDesktopServices.openUrl(QUrl('https://github.com/wuyue92tree/nice-you-get'))
 
     def render_parsed_window(self):
         link = self.linkLineEdit.text()
