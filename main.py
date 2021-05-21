2935cdb22e1948302aaace68bfb257c63d174dd8
wuyue <wuyue92tree@163.com>
2021-05-21 14:39:30 +0800

    fix crash when pyinstaller --windowed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
+import io
 import os
-import you_get
 from PySide2.QtWidgets import QApplication
 from windows import MainWindow
 from utils.logger import get_logger
@@ -8,12 +8,19 @@ from utils.logger import get_logger
 logger = get_logger()
 
 
+# fix pyinstaller --windowed issue case by you_get.
+# you_get will modify sys.stdout.buffer, but when packaged, stdout bin 
+# setup to NullWriter at `pyiboot01_bootstrap.py`
+# issue just like: 
+# https://github.com/pyinstaller/pyinstaller/issues/3503
+# https://github.com/pyinstaller/pyinstaller/issues/1883
+if "NullWriter" in str(type(sys.stdout)):
+    sys.stdout.buffer = io.BytesIO()
+
+
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        you_get.main()
-    else:
-        logger.info(f'process start at: {os.getpid()}')
-        app = QApplication(sys.argv)
-        main_window = MainWindow()
-        main_window.show()
-        sys.exit(app.exec_())
+    logger.info(f'process start at: {os.getpid()}')
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec_())
