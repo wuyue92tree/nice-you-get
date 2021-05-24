4e6d11ded9b6f8a7ac81aca3cce03c86df7ae894
wuyue <wuyue92tree@163.com>
2021-05-24 19:29:56 +0800

    change log dir

--- conf/settings.py
+++ conf/settings.py
@@ -9,8 +9,13 @@ if sys.platform == 'win32':
 else:
     HOME_DIR = os.path.join(os.environ['HOME'], '.nice_you_get')
 
+LOG_DIR = os.path.join(HOME_DIR, 'log')
+
 if os.path.exists(HOME_DIR) is False:
     os.makedirs(HOME_DIR)
+
+if os.path.exists(LOG_DIR) is False:
+    os.makedirs(LOG_DIR)
 
 CONFIG_PATH = os.path.join(HOME_DIR, 'config.json')
 
--- utils/logger.py
+++ utils/logger.py
@@ -2,7 +2,7 @@ import os
 import logging
 import logging.handlers
 import datetime
-from conf.settings import HOME_DIR
+from conf.settings import LOG_DIR
 
 
 def get_logger():
@@ -12,11 +12,11 @@ def get_logger():
     console_hander.setFormatter(logging.Formatter("%(asctime)s %(levelname)s: %(message)s"))
     console_hander.setLevel(logging.INFO)
 
-    rf_handler = logging.handlers.TimedRotatingFileHandler(os.path.join(HOME_DIR, 'all.log'), when='midnight', interval=1, backupCount=10, atTime=datetime.time(0, 0, 0, 0))
+    rf_handler = logging.handlers.TimedRotatingFileHandler(os.path.join(LOG_DIR, 'all.log'), when='midnight', interval=1, backupCount=10, atTime=datetime.time(0, 0, 0, 0))
     rf_handler.setFormatter(logging.Formatter("%(asctime)s %(filename)s %(funcName)s %(processName)s %(threadName)s [line:%(lineno)d] %(levelname)s: %(message)s"))
     rf_handler.setLevel(logging.DEBUG)
 
-    f_handler = logging.FileHandler(os.path.join(HOME_DIR, 'error.log'))
+    f_handler = logging.FileHandler(os.path.join(LOG_DIR, 'error.log'))
     f_handler.setFormatter(logging.Formatter("%(asctime)s %(filename)s %(funcName)s %(processName)s %(threadName)s [line:%(lineno)d] %(levelname)s: %(message)s"))
     f_handler.setLevel(logging.ERROR)
 
